d97ffed6b3ac95f7976e9c87796a7ca4c1bb4ab6
inhHieu <96277218+inhHieu@users.noreply.github.com>
2024-04-25 16:21:21 +0700

    Auth using Supabase

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { FormEvent } from 'react'
+import { useRouter } from 'next/router'
+ 
+export default function SignIp() {
+  
+  return (
+    <h1>
+      Sign In
+    </h1>
+  )
+}
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,52 @@
+
+// import { signup } from '../../action/auth'
+
+import { createClient } from "@/utils/supabase/server";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+ 
+
+
+export default function SignUp() {
+  const  signup = async ()=>{
+    'use server';
+    // 1. create supabase client
+    const supabase = createClient();
+    const origin = headers().get('orgin');
+
+    // 2. sign in with provider
+    const {error, data} = await supabase.auth.signInWithOAuth({
+      provider:'github',
+      options:{
+        redirectTo: `${origin}/auth/callback`
+      }
+    })
+    if(error){
+      console.log(error)
+    }else {
+      return redirect(data.url)
+    }
+    // 3. redirect back
+  }
+  
+  return (
+    <div>
+      <form action={signup}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input className="text-black" id="name" name="name" placeholder="Name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input className="text-black" id="email" name="email" type="email" placeholder="Email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input className="text-black" id="password" name="password" type="password" />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  )
+}
