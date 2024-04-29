0b4417fb3e425ed01fd10ede31307344bd9c204d
inhHieu <96277218+inhHieu@users.noreply.github.com>
2024-04-29 14:12:59 +0700

    fix bug

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,8 @@
-import { NextResponse } from "next/server";
 import { type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
-export function middleware(request: NextRequest) {
-  const requestHeaders = new Headers(request.headers);
-  requestHeaders.set("next-url", request.nextUrl.pathname);
-
-  return (
-    NextResponse.next({
-      request: {
-        headers: requestHeaders,
-      },
-    }),
-    updateSession(request)
-  );
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
 }
 
 export const config = {
