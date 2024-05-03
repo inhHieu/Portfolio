bb073dbde0e30a52be98f6671f9b0e32377a4d42
inhHieu <96277218+inhHieu@users.noreply.github.com>
2024-05-03 17:25:12 +0700

    hope this last fixinggg

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,36 +3,35 @@ import { NextResponse } from "next/server";
 import { type CookieOptions, createServerClient } from "@supabase/ssr";
 
 export async function GET(request: Request) {
-	const { searchParams } = new URL(request.url);
-	const code = searchParams.get("code");
-	// if "next" is in param, use it as the redirect URL
-	const next = searchParams.get("next") ?? "/";
-	const origin = process.env.MANUAL_ORIGIN
-	if (code) {
-		const cookieStore = cookies();
-		const supabase = createServerClient(
-			process.env.NEXT_PUBLIC_SUPABASE_URL!,
-			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-			{
-				cookies: {
-					get(name: string) {
-						return cookieStore.get(name)?.value;
-					},
-					set(name: string, value: string, options: CookieOptions) {
-						cookieStore.set({ name, value, ...options });
-					},
-					remove(name: string, options: CookieOptions) {
-						cookieStore.delete({ name, ...options });
-					},
-				},
-			}
-		);
-		const { error } = await supabase.auth.exchangeCodeForSession(code);
-		if (!error) {
-			return NextResponse.redirect(`https://www.google.com`);
-		}
-	}
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get("code");
+  // if "next" is in param, use it as the redirect URL
+  const next = searchParams.get("next") ?? "/";
+  if (code) {
+    const cookieStore = cookies();
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          get(name: string) {
+            return cookieStore.get(name)?.value;
+          },
+          set(name: string, value: string, options: CookieOptions) {
+            cookieStore.set({ name, value, ...options });
+          },
+          remove(name: string, options: CookieOptions) {
+            cookieStore.delete({ name, ...options });
+          },
+        },
+      }
+    );
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error) {
+      return NextResponse.redirect(`${origin}${next}`);
+    }
+  }
 
-	// return the user to an error page with instructions
-	return NextResponse.redirect(`https://www.google.com`);
+  // return the user to an error page with instructions
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
